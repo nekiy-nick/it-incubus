825ad9a0655b120f6954541fb6556db913d3f13d
KhitrovNickolai <deliriumbred@yandex.ru>
2022-08-16 18:45:06 +0300

    Make a draft of rabbit-body

--- ENTRANT/00 registration/js/script.js
+++ ENTRANT/00 registration/js/script.js
@@ -1,10 +1,21 @@
 let headerBlock = document.querySelector('.block-header');
 
-let carrotBtnList = headerBlock.querySelectorAll('.block-header-ui-list__btn_carrot');
+let carrotBtnList = headerBlock.querySelectorAll('.block-header-ui-list__btn');
+
+const rabbitBody = document.querySelector('.rabbit-body');
+
+const letsFatRabbit = function() {
+	let widthRabbit = rabbitBody.style.width;
+
+	alert(widthRabbit)
+}
 
 carrotBtnList.forEach( (item) => {
 	item.addEventListener('click', (e) => {
-		console.log(item)
+
+		let itemBall = item.dataset.point;
+		letsFatRabbit()
 	})
 })
+
 
