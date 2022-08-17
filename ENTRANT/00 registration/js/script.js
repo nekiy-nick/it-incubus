327c6e616b3b8dc36d5f6025d09a07bc79f94ecf
KhitrovNickolai <deliriumbred@yandex.ru>
2022-08-17 16:47:45 +0300

    Edit rabbit block

--- ENTRANT/00 registration/js/script.js
+++ ENTRANT/00 registration/js/script.js
@@ -2,20 +2,21 @@ let headerBlock = document.querySelector('.block-header');
 
 let carrotBtnList = headerBlock.querySelectorAll('.block-header-ui-list__btn');
 
-const rabbitBody = document.querySelector('.rabbit-body');
+const rabbitBody = document.querySelector('.rabbit-body__belly');
 
-const letsFatRabbit = function() {
-	let widthRabbit = rabbitBody.style.width;
-
-	alert(widthRabbit)
-}
+const letsFatRabbit = function(plusFat) {
+	let widthRabbit = rabbitBody.offsetWidth;
+	if (widthRabbit > 110) rabbitBody.style.width = '0px';
+	alert(plusFat)
+	rabbitBody.style.width += +widthRabbit + +plusFat + 'px';
+};
 
 carrotBtnList.forEach( (item) => {
 	item.addEventListener('click', (e) => {
 
 		let itemBall = item.dataset.point;
-		letsFatRabbit()
-	})
-})
 
+		letsFatRabbit(itemBall)
+	})
+});
 
