1e0b949a4bd77890b0e9a0bee4254a036aae855c
KhitrovNickolai <deliriumbred@yandex.ru>
2022-11-17 16:37:43 +0300

    Edit functional of task

--- ENTRANT/01 codewars tasks/00/script.js
+++ ENTRANT/01 codewars tasks/00/script.js
@@ -1,13 +1,37 @@
 function sortIt(arr) {
-  const copyArr = [...arr].sort((a, b) => a - b).map(num => String(num));
-  let middleResultArr = [];
-  
+  const copyArr = [...arr].sort((a, b) => a - b);
+  const counterMatches = {};
+  const lengthMatchesArray = [];
+
+  const makeSubArray = (value, lot) => {
+  	const subArray = [];
+  	let i = lot;
+
+  	while (i !== 0) {
+  		subArray.push(value);
+  		i--;
+  	}
+
+  	lengthMatchesArray.push(subArray);
+  };
+
   for (let value of copyArr) {
-    // console.log(typeof value)
-//     middleResultArr.push(value.matchAll(`/${value}/g`));
+    if (counterMatches[value] === undefined) {
+    	counterMatches[value] = 1;
+    } else {
+    	counterMatches[value]++;
+    }
   }
-  
-  console.log(copyArr)
+
+  for (value in counterMatches) {
+  	makeSubArray(value, counterMatches[value]);
+  }
+
+  const sortByLengthArray = lengthMatchesArray.sort((prev, next) => prev.length - next.length);
+
+  const resultArray = [];
+
+  console.log(resultArray)
 }
 
 sortIt([1,3,3,5,2,2,4,6,6,7,7,7]);
